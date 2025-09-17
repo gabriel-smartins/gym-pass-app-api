1126b864de940112f34e441053b79cbca49c4ea7
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-17 16:18:45 -0300

    chore: configure test environmet database

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,40 @@
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 import { Environment } from "vitest";
+
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generatedatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environmet variable");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   async setup() {
-    console.log("Setup");
+    const schema = randomUUID();
+
+    const databaseUrl = generatedatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
-        console.log("teardown");
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
       },
     };
   },
