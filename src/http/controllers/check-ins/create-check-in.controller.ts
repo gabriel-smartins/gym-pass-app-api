49b777863987ac8cbeea096acf3710263c7917bb
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-18 16:58:01 -0300

    feat: add create check-in controller and his test

---
+++ src/http/controllers/check-ins/create-check-in.controller.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+
+import { makeCheckInService } from "@/services/factories/make-check-in-service";
+
+export async function createCheckIns(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const createCheckInsParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  });
+
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { gymId } = createCheckInsParamsSchema.parse(request.params);
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body);
+
+  const checkInService = makeCheckInService();
+
+  await checkInService.execute({
+    gymId,
+    userId: request.user.sub,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  reply.status(201).send();
+}
