eae884ddf26ced665b9b6429572e0a063e7b1921
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-18 17:02:36 -0300

    feat: add create gyms controller test and profile controller test

---
+++ src/http/controllers/gyms/create-gyms.controller.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/tests/create-and-authenticate-user";
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app, true);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Free Form",
+        description: "some description",
+        phone: "921091209102",
+        latitude: -15.9514624,
+        longitude: -48.2607104,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
